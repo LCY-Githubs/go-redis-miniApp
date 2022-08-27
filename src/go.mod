786d9114d76fb5b5ddb26888ab95b57400065d5c
lcy <983813520@qq.com>
2022-08-27 23:54:49 +0800

    translate

--- src/go.mod
+++ src/go.mod
@@ -1,1 +1,5 @@
 module src
+
+go 1.18
+
+require github.com/garyburd/redigo v1.6.3 // indirect
---
+++ src/init/conn.go
@@ -0,0 +1,18 @@
+package init
+
+import (
+	"github.com/go-redis/redis"
+)
+
+//定义一个全局的rdb变量
+var rdb *redis.Client
+
+//初始化链接
+func initClient() error {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	return nil
+}
